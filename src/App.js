5075b1aee08d566375c833abc7bd15f04931527f
SruangsureePK <sruansuree.paksa@gmail.com>
2020-09-16 19:23:07 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import WordCard from './WordCard';
 function App() {
   const word = "Hello";
   return (
+    <div>
+      <img src = '4262326.jpg'>
+      <div>
     <div className="App">
       <div>
         {
